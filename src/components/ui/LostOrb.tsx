9d538dd40ac29ed9d1f15b67b736c0fb30e42ced
kavyp12 <92688681+kavyp12@users.noreply.github.com>
2025-06-06 11:47:24 +0530

    Initial commit

--- src/components/ui/LostOrb.tsx
+++ src/components/ui/LostOrb.tsx
@@ -4,10 +4,11 @@ export default function LostOrb() {
   return (
     <div className='w-full h-full'>
       <Spline
-        scene="https://prod.spline.design/V4npIVf9qMJnFSEW/scene.splinecode" 
+        scene="https://prod.spline.design/kVbOOfwISRC36KJd/scene.splinecode" 
   // https://prod.spline.design/ZEmlYLt9o6XFyMZ8/scene.splinecode
         // "https://prod.spline.design/V4npIVf9qMJnFSEW/scene.splinecode"
         // https://prod.spline.design/kVbOOfwISRC36KJd/scene.splinecode -->sun  
+        // https://prod.spline.design/XbAlK8HzVJz-Nif5/scene.splinecode
       />
     </div>
   );
